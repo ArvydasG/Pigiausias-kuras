bc72086d5b2e1c78bb78f44c8598919373df0210
ArvydasG <gailiunas.sapa@gmail.com>
2026-06-22 14:21:06 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'kuro-kainos-v7'; // Versija atnaujinama, kad priverstų SW persikrauti
+const CACHE_NAME = 'kuro-kainos-v8'; // Versija atnaujinama, kad priverstų SW persikrauti
 const urlsToCache = [
   './',
   './index.html',
